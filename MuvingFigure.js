e4e453cc23b2cd188678c5021ca2e84554ffc7b2
Roman Chornyi <ppgora352@gmail.com>
2022-11-23 23:37:03 +0200

    перше виправлення_2

--- MuvingFigure.js
+++ MuvingFigure.js
@@ -176,10 +176,9 @@ drawCoordinatesScale(myScale);
 let A_x = 1, A_y = -1, C_x = 2, C_y = -2;
 let A = new Point(A_x * myScale, A_y * myScale);
 let C = new Point(C_x * myScale, C_y * myScale);
+let square = determiningTheCoordinatesOfTheSquare(A, C, myScale)
 function draw() {
     if (canvas.getContext) {
-        let square = determiningTheCoordinatesOfTheSquare(A, C, myScale)
-
         if(chooseVariant.value === '') {
             ctx.restore();
             ctx.clearRect(0, 0, canvas.width, canvas.height);
